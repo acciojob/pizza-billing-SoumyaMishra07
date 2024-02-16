c3e6ab1cb7f0bea1c1a2a3a18de70c83484caed4
SoumyaMishra07 <soumyarmishra065@gmail.com>
2024-02-16 13:32:30 +0530

    added

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -5,5 +5,7 @@ public class DeluxePizza extends Pizza {
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
         // your code goes here
+        this.price = isVeg ? 300 : 400; // set base price for deluxe pizza
+        this.bill = "Base Price Of The Pizza: " + this.price + "\n";
     }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -2,13 +2,18 @@ package com.driver;
 
 public class Pizza {
 
-    private int price;
-    private Boolean isVeg;
-    private String bill;
+    protected int price;
+    protected Boolean isVeg;
+    protected String bill;
+    protected boolean extraCheeseAdded;
+    protected boolean extraToppingsAdded;
+    protected boolean paperBagAdded;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        this.price = isVeg ? 300 : 400; // set base price for regular pizza
+        this.bill = "Base Price Of The Pizza: " + this.price + "\n";
     }
 
     public int getPrice(){
@@ -17,18 +22,34 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if (!extraCheeseAdded) {
+            this.price += 80;
+            this.bill += "Extra Cheese Added: 80\n";
+            extraCheeseAdded = true;
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if (!extraToppingsAdded) {
+            this.price += isVeg ? 70 : 120;
+            this.bill += "Extra Toppings Added: " + (isVeg ? 70 : 120) + "\n";
+            extraToppingsAdded = true;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        if (!paperBagAdded) {
+            this.price += 20;
+            this.bill += "Paperbag Added: 20\n";
+            paperBagAdded = true;
+        }
     }
 
     public String getBill(){
         // your code goes here
+        this.bill += "Total Price: " + this.price + "\n";
         return this.bill;
     }
 }
