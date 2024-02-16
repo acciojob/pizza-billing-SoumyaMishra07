6eecff2d3d8ffa5099a3d9d59c6507faeb2f0864
SoumyaMishra07 <soumyarmishra065@gmail.com>
2024-02-16 18:26:16 +0530

    initial changes

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -1,7 +1,6 @@
 package com.driver;
 
 public class Pizza {
-
     protected int price;
     protected Boolean isVeg;
     protected String bill;
@@ -9,19 +8,21 @@ public class Pizza {
     protected boolean extraToppingsAdded;
     protected boolean paperBagAdded;
 
-    public Pizza(Boolean isVeg){
+    public Pizza(Boolean isVeg)
+    {
         this.isVeg = isVeg;
-
         this.price = isVeg ? 300 : 400;
         this.bill = "Base Price Of The Pizza: " + this.price + "\n";
     }
 
-    public int getPrice(){
+    public int getPrice()
+    {
         return this.price;
     }
 
     public void addExtraCheese(){
-        if (!extraCheeseAdded) {
+        if (!extraCheeseAdded)
+        {
             this.price += 80;
             this.bill += "Extra Cheese Added: 80\n";
             extraCheeseAdded = true;
@@ -29,23 +30,32 @@ public class Pizza {
     }
 
     public void addExtraToppings(){
-        if (!extraToppingsAdded) {
+        if (!extraToppingsAdded)
+        {
             this.price += isVeg ? 70 : 120;
             this.bill += "Extra Toppings Added: " + (isVeg ? 70 : 120) + "\n";
             extraToppingsAdded = true;
         }
     }
 
-    public void addTakeaway(){
-        if (!paperBagAdded) {
+    public void addTakeaway()
+    {
+        if (!paperBagAdded)
+        {
             this.price += 20;
             this.bill += "Paperbag Added: 20\n";
             paperBagAdded = true;
         }
     }
 
-    public String getBill(){
-        this.bill += "Total Price: " + this.price + "\n";
+    boolean isBill = false;
+    public String getBill()
+    {
+        if(isBill == false){
+            isBill = true;
+            this.bill += "Total Price: " + this.price + "\n";
+            return this.bill;
+        }
         return this.bill;
     }
 }
